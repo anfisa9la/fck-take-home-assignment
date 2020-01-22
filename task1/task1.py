9ca20d17dfaa6ad78a2ad46d668bdf2d6a492c04
rzalegal <vsimanov@opinionsn.com>
2020-01-22 22:42:44 +0300

    task1 completed

--- task1/task1.py
+++ task1/task1.py
@@ -1,4 +1,4 @@
-array = [5, 8, 0 ,0, 1, 5, 5, 5, 10, 6]
+import sys
 
 def sum(arr):
 	acc = 0
@@ -11,49 +11,59 @@ def med(arr):
 	l = len(arr)
 
 	if l % 2 != 0:
-		return arr[l / 2 + 1]
-	return (arr[l / 2] + arr[l / 2 + 1]) / 2.0
+		return arr[l // 2 + 1]
 
+	return (arr[l // 2] + arr[l // 2 + 1]) / 2.0
 
 def maximum(arr):
 	arr = sorted(arr)
 	return arr[-1]
 
-
 def minimum(arr):
 	arr = sorted(arr)
 	return arr[0]
 
-
 def avg(arr):
-	return sum(arr) / float(len(arr))
-
+	return sum(arr) / len(arr)
 
 def percentile(p, arr):
 	from math import modf
 	l = len(arr)
 	arr = sorted(arr)
-	rem, quot = modf(float(p) / 100 * (l - 1) + 1)
-	i = int(quot)
-	try:
-		return arr[i] + rem * (arr[i + 1] - arr[i]) 
-	except IndexError:
-		return arr[i] + rem 
+	rem, quot = modf(p / 100 * (l - 1) + 1)
+	i = int(quot) - 1
+	return arr[i] + rem * (arr[i + 1] - arr[i]) 
+
 
 def percentile90(arr):
 	return percentile(90, arr)
 
-
 def out(arg, *funcs):
 	for f in funcs:
-		print(f.__name__, f(arg))
+		print('%.2f' % f(arg))
 
-out(array,
-	percentile90,
-	med,
-	maximum,
-	minimum,
-	avg
-	)
+
+# def out(arg, *funcs):
+# 	for f in funcs:
+# 		print(f.__name__, '%.2f' % f(arg))
+
+def main(file):
+	rf = open(file, 'r').readlines()
+	array = [int(i) for i in rf]
+
+	out(array,
+		percentile90,
+		med,
+		maximum,
+		minimum,
+		avg
+		)
+
+
+if __name__ == "__main__":
+	main(sys.argv[1])
+
+
+
 
 
